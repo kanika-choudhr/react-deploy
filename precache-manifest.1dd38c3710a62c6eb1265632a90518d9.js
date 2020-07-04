4f5a6f32cbf1fcf3692e79892675009719220844
Choudhri <kanika.choudhri@capgemini.com>
2020-07-04 21:30:51 +0530

    Updates

---
+++ precache-manifest.1dd38c3710a62c6eb1265632a90518d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5f3273ad7dfd27e45c86b6491945b1",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "22013ee4fa6cc0d870d1",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22013ee4fa6cc0d870d1",
+    "url": "/react-deploy/static/js/main.72e2374c.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
