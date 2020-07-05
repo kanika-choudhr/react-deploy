7d7163a10e5d844752cab5e1d82f16713077115d
Choudhri <kanika.choudhri@capgemini.com>
2020-07-05 16:28:23 +0530

    show according to time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const CurrentDate = new Date().toLocaleDateString();
+const CurrentTime = new Date().toLocaleTimeString();
+const today = new Date(2020, 5, 2, 20).getHours();
+let greet = ''
+const cssStyle = { };
+
+if(today > 1 && today < 11){
+  greet= "Hello Sir, Good Morning"
+  cssStyle.color = "green"
+} else if(today >12 && today < 19){
+  greet = "Hello Sir, Good AfterNoon"
+  cssStyle.color = "yellow"
+}else if(today > 19){
+  greet= "Hello Sir, Good Night"
+  cssStyle.color = "blue"
+}
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello {today}</h1>
+  <p style = {cssStyle}>{greet}</p>
+  <p>{CurrentDate}</p>
+  <p>{CurrentTime}</p>
     </div>
   );
 }
